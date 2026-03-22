3d7a761fff5eefff64b7aaa836feec57661da5c4
Ritesh Patel <riteshzeronity@gmail.com>
2026-03-22 17:55:48 +0530

    vertex array object and the first triangle :)

--- main.cpp
+++ main.cpp
@@ -45,10 +45,13 @@ int main() {
         0.0f,  0.5f, 0.0f
     };
 
-    unsigned int VBO;
+    unsigned int VBO, VAO;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
 
     const char *vertexShaderSource = "#version 330 core\n"
         "layout (location = 0) in vec3 aPos;\n"
@@ -105,7 +108,11 @@ int main() {
     glUseProgram(shaderProgram);
 
     glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);  
+    glDeleteShader(fragmentShader);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    
 
     while (!glfwWindowShouldClose(window))
     {
@@ -113,6 +120,10 @@ int main() {
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
